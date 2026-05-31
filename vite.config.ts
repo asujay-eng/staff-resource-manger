c3eb3b03e5b93f5147a5ba60e5a86eabd39bc715
asujay-eng <asujay@gmail.com>
2026-05-31 23:14:07 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: true
+  server: {
+    host: "0.0.0.0",
+    allowedHosts: [
+      "staff-resource-manger-production.up.railway.app"
+    ]
   }
-})
+});
