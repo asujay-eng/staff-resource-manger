8b6c3b4f171718b666a58a4aa67b623116ee4cba
asujay-eng <asujay@gmail.com>
2026-06-01 00:09:56 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
 import { useState } from "react";
 import StaffForm from "./StaffForm";
 import ResourceManager from "./ResourceManager";
+import DashboardMap from "./DashboardMap";
 import "./App.css";
 import "./StaffForm.css";
 
 export default function App() {
-  const [page, setPage] = useState<"staff" | "admin">("staff");
+  const [page, setPage] = useState<"staff" | "admin" | "dashboard">("staff");
 
   return (
     <>
       <div className="top-nav">
         <button onClick={() => setPage("staff")}>Staff Form</button>
         <button onClick={() => setPage("admin")}>Resource Manager</button>
+        <button onClick={() => setPage("dashboard")}>Dashboard</button>
       </div>
 
-      {page === "staff" ? <StaffForm /> : <ResourceManager />}
+      {page === "staff" && <StaffForm />}
+      {page === "admin" && <ResourceManager />}
+      {page === "dashboard" && <DashboardMap />}
     </>
   );
 }
